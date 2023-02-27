9db632de03ef9395bf23dc5e6ce4afe80fc21663
malolasimman <malolasimman7101999@gmail.com>
2023-02-27 06:58:11 +0000

    final

--- runqemu.sh
+++ runqemu.sh
@@ -12,4 +12,4 @@ qemu-system-aarch64 \
     -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
     -device virtio-net-device,netdev=eth0 \
     -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
-    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
+    -device virtio-blk-device,drive=hd0 -device virtio-rng-pci 
