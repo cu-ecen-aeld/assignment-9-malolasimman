4349c861885a369865b1a9f1fdb6415ecbcfa557
malolasimman <malolasimman7101999@gmail.com>
2023-03-19 02:59:13 +0000

    updated code

---
+++ base_external/package/aesd_ldd/aesd-ldd.mk
@@ -0,0 +1,23 @@
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_LDD_VERSION = a83f1c6246716dd44a9864d9435d6582b43f0556
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_LDD_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-malolasimman.git
+AESD_LDD_SITE_METHOD = git
+AESD_LDD_GIT_SUBMODULES = YES
+
+AESD_LDD_MODULE_SUBDIRS = aesd-char-driver/
+
+# TODO adding all the dependencies.
+define AESD_LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
